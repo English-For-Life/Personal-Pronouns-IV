91dff465750b8cb7abd908f0712cae44aa35d166
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 12:48:19 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"Do you like apples? - I love ___ .",
+        options:["it", "them", "they"],
+        answer: 1
+    },
+    {
+     q:"My mum is wonderful. I love ___ very much.",
+        options:["her", "she", "his"],
+        answer: 0   
+    },
+    {
+        q:"Your parents were so kind to me. Thank ___ , please.",
+        options:["they", "him", "them"],
+        answer: 2
+    },
+    {
+        q:"Where is my phone? Oh, dear! I've lost ___ . ",
+        options:[ "them", "him", "it"],
+        answer: 2
+    },
+    {
+        q:"You didn't come to the party. What happened to ___ ?",
+        options:["you", "your", "it",],
+        answer: 0
+    },
+    {
+        q:"I have to finish homework. Can you help ___ ?",
+        options:["I", "me", "it"],
+        answer: 1
+    },
+    {
+        q:"My father is fantastic! I'm proud of ___ .",
+        options:["him", "he", "his"],
+        answer: 0
+    },
+    {
+        q:"The kids are making so much noise. Can you tell ___ to be quiet?",
+        options:["him", "them", "they"],
+        answer: 1
+    },
+    {
+        q:"Look at the flower. You should water ___ .",
+        options:["him", "it", "them"],
+        answer: 1
+    },
+    {
+        q:"Oh, dear! There is a mouse under the table. I'm afraid of ___ .",
+        options:["them", "her", "it"],
+        answer: 2
+    }
+
+]
